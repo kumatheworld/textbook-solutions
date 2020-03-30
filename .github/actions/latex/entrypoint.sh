bc508dbc55a3605e21145e27521c66a390054481
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-03-30 21:55:14 +0900

    Create .github for generating .pdf

---
+++ .github/actions/latex/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eux
+
+name=TheFoundationsOfMathematics
+
+# build pdf
+ptex2pdf -l -ot -kanji=utf8 $directory/solutions.tex
+
+# create release
+res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
+-d "
+{
+  \"tag_name\": \"$(echo ${GITHUB_REF:10})\",
+  \"target_commitish\": \"$GITHUB_SHA\",
+  \"name\": \"solutions.pdf $(echo ${GITHUB_REF:10})\",
+  \"draft\": false,
+  \"prerelease\": false
+}"`
+
+# extract release id
+rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
+
+# upload built pdf
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=$name.pdf\
+  --header 'Content-Type: application/pdf'\
+  --upload-file $name.pdf
