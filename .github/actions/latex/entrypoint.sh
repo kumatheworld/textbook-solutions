ee080276593726a6c447a1bc7f9c81af9a4df4a3
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-04-26 09:01:19 +0900

    Fix bug

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -2,9 +2,11 @@
 set -eux
 
 name=TheFoundationsOfMathematics
+tex=$name/solutions.tex
+pdf=$name.pdf
 
 # build pdf
-ptex2pdf -l -ot -kanji=utf8 $directory/solutions.tex
+pdflatex $tex -job-name=$pdf
 
 # create release
 res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
@@ -12,15 +14,15 @@ res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com
 {
   \"tag_name\": \"$(echo ${GITHUB_REF:10})\",
   \"target_commitish\": \"$GITHUB_SHA\",
-  \"name\": \"($echo $name.pdf) $(echo ${GITHUB_REF:10})\",
+  \"name\": \"$(echo $pdf) $(echo ${GITHUB_REF:10})\",
   \"draft\": false,
   \"prerelease\": false
 }"`
 
 # extract release id
-rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
+rel_id=`echo $res | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
 
 # upload built pdf
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=$name.pdf\
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$rel_id/assets?name=$pdf\
   --header 'Content-Type: application/pdf'\
-  --upload-file $name.pdf
+  --upload-file $pdf
