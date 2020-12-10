a587c97b90bbbe65b7fca6268baab8eeb1c280b1
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-12-10 21:56:50 +0900

    Correct entrypoint.sh

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -eux
 
+# create release
+res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
+-d "
+{
+  \"tag_name\": \"$(echo ${GITHUB_REF:10})\",
+  \"target_commitish\": \"$GITHUB_SHA\",
+  \"name\": \"$(echo ${GITHUB_REF:10})\",
+  \"draft\": false,
+  \"prerelease\": false
+}"`
+
+# extract release id
+rel_id=`echo $res | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
+
 names=(TheFoundationsOfMathematics SetTheory)
 for name in ${names[@]}; do
   tex=$name/solutions.tex
@@ -8,20 +22,6 @@ for name in ${names[@]}; do
 
   # build pdf
   pdflatex -jobname=$name $tex
-
-  # create release
-  res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
-  -d "
-  {
-    \"tag_name\": \"$(echo ${GITHUB_REF:10})\",
-    \"target_commitish\": \"$GITHUB_SHA\",
-    \"name\": \"$(echo $pdf) $(echo ${GITHUB_REF:10})\",
-    \"draft\": false,
-    \"prerelease\": false
-  }"`
-
-  # extract release id
-  rel_id=`echo $res | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
 
   # upload built pdf
   curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$rel_id/assets?name=$pdf\
